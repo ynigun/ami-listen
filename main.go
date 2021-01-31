ca3f4449a9aabb7f576a140447e91425cc0a5e85
ynigun <30502350+ynigun@users.noreply.github.com>
2021-01-31 16:14:36 +0200

    Update main.go

--- main.go
+++ main.go
@@ -12,21 +12,33 @@ func main() {
  	user := "asterisk"
   	pass :=  "asterisk"
   	ip := "127.0.0.1"
-	
+	//יצירת חיבור לAMI
 	a := amichan.New(user,pass,ip , port, keepalive)
 	a.Connect()
 
 	for {
+		//לולאה אינסופית שמאזינה לAMI
 		select {
 		case err := <-a.Err():
+		// להדפיס לקונסול במקרה של שגיאה	
 			fmt.Println(err)
 		case event := <-a.Event():
+			// במקרה של אירוע לבדוק האם קיבלתי אירוע של הקשות
 			if event.Name() == "DTMFEnd" {
 				
-
-				num, ok := event.Field("CallerIDNum")
+				// בדיקה האם קיים רשומה באובייקט של האירוע
+				// הפונקציה מחזירה שתי ערכים
+				//1. האם האובייקט קיים
+				// 2. את הערך באובייקט
+				// כיוון שאסור (לפעמים) להשתמש באבייקט ריק
+				// אז אני קודם בודק האם קיבלתי האם האובייקט קיים
+				// ורק אז משתמש בו
+				// אז כאן אני מדפיס את המספר שלך המאזין
+				// ואת המקש שהוא הקיש
+				// לא זוכר מה עשיתי עם הערך השלישי
+				Num, ok := event.Field("CallerIDNum")
 				if ok {
-					fmt.Println(num)
+					fmt.Println(Num)
 				}
         
 				Digit, ok := event.Field("Digit")
